019e721a6c227db92a635c4553129677d0aef33f
Paul <p.rossiter83@gmail.noreply.github.com>
2024-06-21 14:29:19 +0100

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+/* GET new message form. */
+router.get('/new', function (req, res, next) {
+  res.render('form', {title: "New Message"});
+});
+
+/* POST new message. */
+router.post('/new', function(req, res, next) {
+  const messageText = req.body.messageText;
+  const messageUser = req.body.messageUser;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
+});
+module.exports = router;
