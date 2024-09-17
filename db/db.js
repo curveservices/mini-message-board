e4fa750e5a432d789cbf7a8a2b5cc6c0d22b9383
Paul <p.rossiter83@gmail.noreply.github.com>
2024-09-17 14:52:00 +0100

    Env changes

--- db/db.js
+++ db/db.js
@@ -2,12 +2,6 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
+module.exports = new Pool({
+    connectionString: process.env.db_connection
 });
-
-module.exports = pool;
